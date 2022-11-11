4da037a424d8520b6a393de69e9352d4c5963957
Jafar Kasymov <116582499+JafarKasimov@users.noreply.github.com>
2022-11-12 02:14:50 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,33 @@
+public abstract class Hero implements HavingSuperAbility {
+
+    private int health;
+    private int damage;
+    private String typeOfSuperpower;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        damage = damage;
+    }
+
+    public String getTypeOfSuperpower() {
+        return typeOfSuperpower;
+    }
+
+    public void setTypeOfSuperpower(String typeOfSuperpower) {
+        this.typeOfSuperpower = typeOfSuperpower;
+    }
+
+    public abstract void applySuperAbility(String superAbilityType);
+}
+
